319caf3683832a546c7cea93f54f3fdf11693304
Srikanth Vangala <Srikanth.Vangala@phoenix.edu>
2020-09-04 14:16:09 -0700

    v3

--- api/health.js
+++ api/health.js
@@ -11,7 +11,7 @@ exports.handler = async (event) =>{
             headers= util.getResponseHeaders(),
             body: JSON.stringify({
                 message: "Healthy CI/CD Demo",
-                version: "v2.0",
+                version: "v3.0",
                 timestamp: moment().unix()
             })
         }
