f94793d81a594bd8418bf90efb8f9b779afb087a
Srikanth Vangala <Srikanth.Vangala@phoenix.edu>
2020-09-04 15:41:08 -0700

    Modified with trycatch

--- api/health.js
+++ api/health.js
@@ -1,18 +1,36 @@
 const AWS = require('aws-sdk');
-const moment =require('moment');
+AWS.config.update({region:'us-west-2'});
 const util =require('./util.js');
 const s3 = new AWS.S3();
-
-AWS.config.update({region:'us-west-2'});
-exports.handler = async (event,context) =>{
-            return{
+ 
+// const dynamodb=new AWS.DynamoDB.DocumentClient();
+// const tableName = process.env.NOTES_TABLE;
+ 
+exports.handler = async (event) =>{
+    try {
+        return{
             statusCode: 200,
             headers: util.getResponseHeaders(),
             body: JSON.stringify({
-                message: "Healthy CI/CD Demo",
-                version: "v3.0",
+            message: "Healthy CI/CD Demo",
+                version: "v1.0",
                 timestamp: moment().unix()
+})
+        }
+ 
+    } catch (err) {
+        console.log("Error",err);
+//Have to return HTTP response with status codes
+        return {
+            statusCode: err.statusCode ? err.StatusCode : 500, 
+            headers: util.getResponseHeaders(),
+            body: JSON.stringify ({
+                error: err.name ? err.name : "Exception",
+                message: err.message? err.message: "unknown error"
             })
         }
+    }
+ 
 }
+
  
