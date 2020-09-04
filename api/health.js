e007e6e274c2bd66021cc053c5f0c35cd5ff09c5
Srikanth Vangala <Srikanth.Vangala@phoenix.edu>
2020-09-04 15:33:25 -0700

    fixed issue

--- api/health.js
+++ api/health.js
@@ -4,11 +4,10 @@ const util =require('./util.js');
 const s3 = new AWS.S3();
 
 AWS.config.update({region:'us-west-2'});
-exports.handler = async (event) =>{
-    
-        return{
-            statusCode= 200,
-            headers= util.getResponseHeaders(),
+exports.handler = async (event,context) =>{
+            return{
+            statusCode: 200,
+            headers: util.getResponseHeaders(),
             body: JSON.stringify({
                 message: "Healthy CI/CD Demo",
                 version: "v3.0",
